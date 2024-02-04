63694612c12a4e3062cdc08886110ce5efbbf233
Paweł <pawel_gnat@o2.pl>
2024-02-04 16:14:51 +0100

    chore: rewrite to nextauth

--- firebase/firestore/addUserInfo.ts
+++ firebase/firestore/addUserInfo.ts
@@ -1,21 +1,21 @@
-import firebase_app from "../config";
-import { getFirestore, doc, setDoc } from "firebase/firestore";
+import firebase_app from '../config'
+import { getFirestore, doc, setDoc } from 'firebase/firestore'
 
-import { UserDataType } from "@/types/types";
+import { UserDataType } from '@/types/types'
 
-const db = getFirestore(firebase_app);
+const db = getFirestore(firebase_app)
 
 export default async function addUserInfo(id: string, data: UserDataType) {
-  let result = null;
-  let error = null;
+	let result = null
+	let error = null
 
-  try {
-    result = await setDoc(doc(db, "usersInfo", id), data, {
-      merge: true,
-    });
-  } catch (e) {
-    error = e;
-  }
+	try {
+		result = await setDoc(doc(db, 'usersInfo', id), data, {
+			merge: true,
+		})
+	} catch (e) {
+		error = e
+	}
 
-  return { result, error };
+	return { result, error }
 }
--- firebase/firestore/getAllInvoices.ts
+++ firebase/firestore/getAllInvoices.ts
@@ -1,30 +1,20 @@
-import firebase_app from "../config";
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  where,
-  query,
-  orderBy,
-} from "firebase/firestore";
+import firebase_app from '../config'
+import { getFirestore, collection, getDocs, where, query, orderBy } from 'firebase/firestore'
 
-const db = getFirestore(firebase_app);
+const db = getFirestore(firebase_app)
 
 export default async function getAllInvoices(userId: string) {
-  let result = null;
-  let error = null;
+	let result = null
+	let error = null
 
-  try {
-    const invoicesCollection = collection(db, "invoices");
-    const userInvoicesQuery = query(
-      invoicesCollection,
-      where("userId", "==", userId)
-    );
-    const snapshot = await getDocs(userInvoicesQuery);
-    result = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-  } catch (e) {
-    error = e;
-  }
+	try {
+		const invoicesCollection = collection(db, 'invoices')
+		const userInvoicesQuery = query(invoicesCollection, where('userId', '==', userId))
+		const snapshot = await getDocs(userInvoicesQuery)
+		result = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+	} catch (e) {
+		error = e
+	}
 
-  return { result, error };
+	return { result, error }
 }
